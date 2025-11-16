55dff5a21927cdbd7231f3d97537d2b7988db898
Yisuscrack210 <jmarq@Yisus>
2025-11-16 11:56:40 +0100

    Proyecto maven compartido

---
+++ it1/src/main/java/BC02/it1/ServicioConsulta.java
@@ -0,0 +1,12 @@
+package BC02.it1;
+
+public class ServicioConsulta {
+	 public MaterialBibliografico buscarMaterial(String criterio) {
+	        RepositorioDelMaterial repo = new RepositorioDelMaterial();
+	        return repo.consultarBD(criterio);
+	    }
+
+	    public MaterialBibliografico formatearRespuesta(MaterialBibliografico material) {
+	        return material; // En caso de querer aplicar transformaciones
+	    }
+}
