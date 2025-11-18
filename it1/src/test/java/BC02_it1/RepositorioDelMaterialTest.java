e72fb2e116c19d67e0297c6755e0aa87517aee96
AINHOA SÁNCHEZ ESTEBAN <Ainhoa.Sanchez5@alu.uclm.es>
2025-11-18 19:50:13 +0100

    pruebas it1

--- it1/src/test/java/BC02_it1/RepositorioDelMaterialTest.java
+++ it1/src/test/java/BC02_it1/RepositorioDelMaterialTest.java
@@ -1,5 +1,10 @@
 package BC02_it1;
-
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 public class RepositorioDelMaterialTest {
-	
+	@Test
+	void conexion() {
+		RepositorioDelMaterial rm = new RepositorioDelMaterial();
+		assertDoesNotThrow(() -> rm.conectar());
+	}
 }
--- it1/src/test/java/BC02_it1/ServicioConsultaTest.java
+++ it1/src/test/java/BC02_it1/ServicioConsultaTest.java
@@ -1,5 +1,25 @@
 package BC02_it1;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 public class ServicioConsultaTest {
-
+	
+	@Test
+	void busquedaMaterial() {
+		ServicioConsulta s = new ServicioConsulta();
+		MaterialBibliografico mb = s.buscarMaterial("Informatica");
+		assertNotNull(mb);
+		assertEquals("Informatica",mb.getDatos());
+		
+	}
+	
+	@Test
+	void formateoRespuesta() {
+		ServicioConsulta s = new ServicioConsulta();
+		MaterialBibliografico mb = new MaterialBibliografico("El Quijote","Cervantes","Novela",1605);
+		
+		MaterialBibliografico mb2 = s.formatearRespuesta(mb);
+		assertSame(mb,mb2); //mismo objeto
+		
+	}
 }
